ecdb112d206f325ba498bb07a0cdd5d9f6d90e50
callmez <callme-z@qq.com>
2014-09-24 14:57:38 +0800

    remove AdminLTE and install it via bower

--- modules/admin/assets/AdminAsset.php
+++ modules/admin/assets/AdminAsset.php
@@ -8,16 +8,12 @@ class AdminAsset extends AssetBundle
 {
     public $sourcePath = '@app/modules/admin/assets/web';
     public $css = [
-        'css/AdminLTE.css',
         'css/admin.css',
     ];
     public $js = [
-        'js/AdminLTE.js',
         'js/admin.js'
     ];
     public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap\bootstrapAsset',
-        'yii\bootstrap\bootstrapPluginAsset'
+        'app\assets\AdminLteAsset'
     ];
 }
